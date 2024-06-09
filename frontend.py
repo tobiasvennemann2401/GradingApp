1021836e7ab944fbd12e8625ec32699509a447f9
Tobias <tobias@vennemann1.de>
2024-06-09 13:41:45 +0200

    add condition selection

--- frontend.py
+++ frontend.py
@@ -11,6 +11,8 @@ def start_dialog():
     st.write(f"Please enter you participant data")
     participant_id = st.number_input('Participant Id', min_value=0, max_value=50, step=1)
     prompt_choice = st.selectbox("Prompt", options=datastructure.prompts)
+    condition = st.selectbox("Condition", options=["Single Grading", "Cluster Grading"])
+    st.session_state['condition'] = condition
     if st.button('Start'):
         session.create_session(participant_id, prompt_choice)
         st.rerun()
@@ -30,87 +32,88 @@ if 'initialized' not in st.session_state:
     st.session_state['clusters'] = [-1]
     st.session_state.cluster_choice = -1
     st.session_state['last_choice'] = -1
+    st.session_state['condition'] = 'Single Grading'
     start_dialog()
 else:
+    if st.session_state['condition'] != 'Single Grading':
+        st.sidebar.header('Clustering Options')
 
-    st.sidebar.header('Clustering Options')
-
-    # Sidebar
-    with st.sidebar.expander("Preprocessing"):
-        expand_contractions = st.checkbox('Expand Contractions',
-                                          help='This checkbox enables the changes from: don´t -> do not')
-        if expand_contractions != st.session_state['expand_contractions']:
-            st.session_state['expand_contractions'] = expand_contractions
-            session.log_button("expand_contractions", expand_contractions)
-        remove_stopwords = st.checkbox('Remove Stopwords',
-                                       help='This checkbox removes certain words as: do, and, is, if')
-        if remove_stopwords != st.session_state['remove_stopwords']:
-            st.session_state['remove_stopwords'] = remove_stopwords
-            session.log_button("remove_stopwords", remove_stopwords)
-        preproc_method = st.radio(
-            "Word Normalization Method",
-            ["Nothing", "Lemmatization", "Stemming"],
-            captions=["Words arent changed", "Words are transformed into there dictionary form",
-                      "Words are cut after the word stem"])
-        if preproc_method != st.session_state['preproc_method']:
-            st.session_state['preproc_method'] = preproc_method
-            session.log_button("preproc_method", preproc_method)
-        st.text("\n\n")
-        st.text("Example Sentence:")
-        with st.container(border=20):
-            sentence = "This sentence is an example of a sentence but hasn't got any other function"
-            st.write(sentence)
+        # Sidebar
+        with st.sidebar.expander("Preprocessing"):
+            expand_contractions = st.checkbox('Expand Contractions',
+                                              help='This checkbox enables the changes from: don´t -> do not')
+            if expand_contractions != st.session_state['expand_contractions']:
+                st.session_state['expand_contractions'] = expand_contractions
+                session.log_button("expand_contractions", expand_contractions)
+            remove_stopwords = st.checkbox('Remove Stopwords',
+                                           help='This checkbox removes certain words as: do, and, is, if')
+            if remove_stopwords != st.session_state['remove_stopwords']:
+                st.session_state['remove_stopwords'] = remove_stopwords
+                session.log_button("remove_stopwords", remove_stopwords)
+            preproc_method = st.radio(
+                "Word Normalization Method",
+                ["Nothing", "Lemmatization", "Stemming"],
+                captions=["Words arent changed", "Words are transformed into there dictionary form",
+                          "Words are cut after the word stem"])
+            if preproc_method != st.session_state['preproc_method']:
+                st.session_state['preproc_method'] = preproc_method
+                session.log_button("preproc_method", preproc_method)
             st.text("\n\n")
-            sentence = nlp_pipeline.clean_text(sentence)
-            if expand_contractions:
-                sentence = nlp_pipeline.to_expand_contractions(sentence)
-            if remove_stopwords:
-                sentence = nlp_pipeline.to_no_stopwords(sentence)
-            if preproc_method == "Stemming":
-                sentence = nlp_pipeline.stem_text(sentence)
-            if preproc_method == "Lemmatization":
-                sentence = nlp_pipeline.lemmatize_text(sentence)
-            session.preprocess(expand_contractions, remove_stopwords, preproc_method)
-            st.text("After Preprocessing")
-            st.write(sentence)
+            st.text("Example Sentence:")
+            with st.container(border=20):
+                sentence = "This sentence is an example of a sentence but hasn't got any other function"
+                st.write(sentence)
+                st.text("\n\n")
+                sentence = nlp_pipeline.clean_text(sentence)
+                if expand_contractions:
+                    sentence = nlp_pipeline.to_expand_contractions(sentence)
+                if remove_stopwords:
+                    sentence = nlp_pipeline.to_no_stopwords(sentence)
+                if preproc_method == "Stemming":
+                    sentence = nlp_pipeline.stem_text(sentence)
+                if preproc_method == "Lemmatization":
+                    sentence = nlp_pipeline.lemmatize_text(sentence)
+                session.preprocess(expand_contractions, remove_stopwords, preproc_method)
+                st.text("After Preprocessing")
+                st.write(sentence)
 
-    with st.sidebar.expander("Non Compliance Check"):
-        non_compliance = st.checkbox('Create Non Compliance Cluster')
-        if non_compliance != st.session_state['non_compliance']:
-            st.session_state['non_compliance'] = non_compliance
-            session.log_button("non_compliance", non_compliance)
-        st.write(
-            "Answers that do not contain any of the top 10 most used words are considered non compliant and put in a separate cluster.")
-        st.text("Top 10 words")
-        st.table(session.get_top_10_words())
+        with st.sidebar.expander("Non Compliance Check"):
+            non_compliance = st.checkbox('Create Non Compliance Cluster')
+            if non_compliance != st.session_state['non_compliance']:
+                st.session_state['non_compliance'] = non_compliance
+                session.log_button("non_compliance", non_compliance)
+            st.write(
+                "Answers that do not contain any of the top 10 most used words are considered non compliant and put in a separate cluster.")
+            st.text("Top 10 words")
+            st.table(session.get_top_10_words())
 
-    with st.sidebar.expander("Clustering"):
-        distance_calculation_method = st.radio(
-            "Distance Calculation Method",
-            ["Character Based", "Token Based"],
-            captions=["Distance is calculated based on how many characters are different",
-                      "Distance is calculated based on how many words are different"])
-        if distance_calculation_method != st.session_state['distance_calculation_method']:
-            st.session_state['distance_calculation_method'] = distance_calculation_method
-            session.log_button("distance_calculation_method", distance_calculation_method)
-        distance_threshold = st.number_input('Distance Threshold', min_value=0, max_value=10, step=1, value=5,
-                                             help='This value determines the maximum distance two answers in one cluster can be apart')
-        if distance_threshold != st.session_state['distance_threshold']:
-            st.session_state['distance_threshold'] = distance_threshold
-            session.log_button("distance_threshold", distance_threshold)
-        filter_negations = st.checkbox('Filter Negations',
-                                       help='Prevents sentences with opposite meanings (e.g., "I like it" vs. "I dont like it") from being grouped together.')
-        if filter_negations != st.session_state['filter_negations']:
-            st.session_state['filter_negations'] = filter_negations
-            session.log_button("filter_negations", filter_negations)
+        with st.sidebar.expander("Clustering"):
+            distance_calculation_method = st.radio(
+                "Distance Calculation Method",
+                ["Character Based", "Token Based"],
+                captions=["Distance is calculated based on how many characters are different",
+                          "Distance is calculated based on how many words are different"])
+            if distance_calculation_method != st.session_state['distance_calculation_method']:
+                st.session_state['distance_calculation_method'] = distance_calculation_method
+                session.log_button("distance_calculation_method", distance_calculation_method)
+            distance_threshold = st.number_input('Distance Threshold', min_value=0, max_value=10, step=1, value=5,
+                                                 help='This value determines the maximum distance two answers in one cluster can be apart')
+            if distance_threshold != st.session_state['distance_threshold']:
+                st.session_state['distance_threshold'] = distance_threshold
+                session.log_button("distance_threshold", distance_threshold)
+            filter_negations = st.checkbox('Filter Negations',
+                                           help='Prevents sentences with opposite meanings (e.g., "I like it" vs. "I dont like it") from being grouped together.')
+            if filter_negations != st.session_state['filter_negations']:
+                st.session_state['filter_negations'] = filter_negations
+                session.log_button("filter_negations", filter_negations)
 
-    if st.sidebar.button('Cluster'):
-        session.cluster(filter_negations, distance_calculation_method == "Token Based", non_compliance,
-                        distance_threshold)
-        st.session_state['clusters'] = session.sort_clusters()
-        st.session_state.cluster_choice = st.session_state['clusters'][0]
-        st.session_state['last_choice'] = st.session_state.cluster_choice
-        session.log_button("cluster")
+        if st.sidebar.button('Cluster'):
+            session.cluster(filter_negations, distance_calculation_method == "Token Based", non_compliance,
+                            distance_threshold)
+            st.session_state['clusters'] = session.sort_clusters()
+            st.session_state.cluster_choice = st.session_state['clusters'][0]
+            st.session_state['last_choice'] = st.session_state.cluster_choice
+            session.log_button("cluster")
 
 
 
@@ -184,10 +187,11 @@ else:
                 st.session_state['last_choice'] = st.session_state.cluster_choice
                 session.log_button("cluster_choice", st.session_state.cluster_choice)
 
-            show_preprocess = st.checkbox('Show Preprocessing Result', key="show_preproc")
-            if show_preprocess != st.session_state['show_preprocess']:
-                st.session_state['show_preprocess'] = show_preprocess
-                session.log_button("show_preprocess", show_preprocess)
+            if st.session_state['condition'] != 'Single Grading':
+                show_preprocess = st.checkbox('Show Preprocessing Result', key="show_preproc")
+                if show_preprocess != st.session_state['show_preprocess']:
+                    st.session_state['show_preprocess'] = show_preprocess
+                    session.log_button("show_preprocess", show_preprocess)
 
             if st.session_state.cluster_choice is not None:
                 filtered_data = session.get_session().student_answers[
@@ -195,7 +199,7 @@ else:
                             session.get_session().student_answers['grade'] == -1)]
                 filtered_data = filtered_data.drop(['grade', 'cluster'], axis=1)
 
-                if show_preprocess:
+                if st.session_state['show_preprocess']:
                     filtered_data = filtered_data.drop(['answer_display'], axis=1)
                 else:
                     filtered_data = filtered_data.drop(['answer'], axis=1)
@@ -240,7 +244,7 @@ else:
         st.download_button(
             "Press to Download Grades CSV",
             grade_csv,
-            f"grades_{session.get_session().id}.csv",
+            f"grades_{session.get_session().id}_{st.session_state['condition'].strip()}.csv",
             "text/csv",
             key='download-grade-csv'
         )
@@ -248,13 +252,14 @@ else:
         st.download_button(
             "Press to Download Log",
             log_csv,
-            f"button_log_{session.get_session().id}.csv",
+            f"button_log_{session.get_session().id}_{st.session_state['condition'].strip()}.csv",
             "text/csv",
             key='download-log-csv'
         )
 
 
     if session.get_progress() >= 1:
-        if st.sidebar.button('Reopen Dialog'):
-            export()
+        export()
+
+    if st.sidebar.button('Open Emergency Dialog'):
         export()
